5944487ad338fc97d2776d5349050761b6cbdc05
Yair Jacobsohn <y.jacobsohn@gmail.com>
2025-12-25 20:42:44 +0200

    consolidate function completed

--- Heap.java
+++ Heap.java
@@ -1,1 +1,207 @@
+import java.util.LinkedList;
+
+/**
+ * Heap
+ *
+ * An implementation of Fibonacci heap over positive integers 
+ * with the possibility of not performing lazy melds and 
+ * the possibility of not performing lazy decrease keys.
+ *
+ */
+public class Heap
+{
+    public final boolean lazyMelds;
+    public final boolean lazyDecreaseKeys;
+    public HeapNode min;
+    public LinkedList<HeapNode> heads;
+    public int HeapSize=0;
+    public int numOfTrees=0;
+    public int numOfLinks=0;
+    
+    /**
+     *
+     * Constructor to initialize an empty heap.
+     *
+     */
+    public Heap(boolean lazyMelds, boolean lazyDecreaseKeys)
+    {
+        this.lazyMelds = lazyMelds;
+        this.lazyDecreaseKeys = lazyDecreaseKeys;
+        // student code can be added here
+    }
+
+    /**
+     * 
+     * pre: key > 0
+     *
+     * Insert (key,info) into the heap and return the newly generated HeapNode.
+     *
+     */
+    public HeapNode insert(int key, String info) 
+    {    
+        return null; // should be replaced by student code
+    }
+
+    /**
+     * 
+     * Return the minimal HeapNode, null if empty.
+     *
+     */
+    public HeapNode findMin()
+    {
+        return null; // should be replaced by student code
+    }
+
+    /**
+     * 
+     * Delete the minimal item.
+     *
+     */
+    public void deleteMin()
+    {
+        return; // should be replaced by student code
+    }
+
+    /**
+     * 
+     * pre: 0<=diff<=x.key
+     * 
+     * Decrease the key of x by diff and fix the heap.
+     * 
+     */
+    public void decreaseKey(HeapNode x, int diff) 
+    {    
+        return; // should be replaced by student code
+    }
+
+    /**
+     * 
+     * Delete the x from the heap.
+     *
+     */
+    public void delete(HeapNode x) 
+    {    
+        return; // should be replaced by student code
+    }
+
+
+    /**
+     * 
+     * Meld the heap with heap2
+     * pre: heap2.lazyMelds = this.lazyMelds AND heap2.lazyDecreaseKeys = this.lazyDecreaseKeys
+     *
+     */
+    public void meld(Heap heap2)
+    {
+        return; // should be replaced by student code           
+    }
+    
+    
+    /**
+     * 
+     * Return the number of elements in the heap
+     *   
+     */
+    public int size()
+    {
+        return 46; // should be replaced by student code
+    }
+
+
+    /**
+     * 
+     * Return the number of trees in the heap.
+     * 
+     */
+    public int numTrees()
+    {
+        return 46; // should be replaced by student code
+    }
+    
+    
+    /**
+     * 
+     * Return the number of marked nodes in the heap.
+     * 
+     */
+    public int numMarkedNodes()
+    {
+        return 46; // should be replaced by student code
+    }
+    
+    
+    /**
+     * 
+     * Return the total number of links.
+     * 
+     */
+    public int totalLinks()
+    {
+        return 46; // should be replaced by student code
+    }
+    
+
+    
+    public void consolidate() {
+    	// helper function unites all same degree trees
+    	// adjust heads so does every tree has unique deg 
+    	// time complexity O(numTrees), outer while runs numTree iterations
+    	// inner while iteration by number of links (blocked by O(numTree) either).
+    	double len = (Math.log(HeapSize)/Math.log(1.5))+1;
+    	HeapNode [] rankArr = new HeapNode[(int) len]; // initialize array length, blocked by log 
+    	HeapNode curr = heads.getFirst();   
+    	while(curr != null) { // running over every treeRoot
+    		while(rankArr[curr.rank] !=null) {   // linking until open spot in the array
+    			curr = curr.link(rankArr[curr.rank]);
+    			this.numOfLinks++;this.numOfTrees--;
+
+    		}
+    		rankArr[curr.rank] =curr;
+    
+    	}
+    	
+    }
+    
+    
+    /**
+     * 
+     * Return the total number of cuts.
+     * 
+     */
+    public int totalCuts()
+    {
+        return 46; // should be replaced by student code
+    }
+    
+
+    /**
+     * 
+     * Return the total heapify costs.
+     * 
+     */
+    public int totalHeapifyCosts()
+    {
+        return 46; // should be replaced by student code
+    }
+    
+    
+    /**
+     * Class implementing a node in a ExtendedFibonacci Heap.
+     *  
+     */
+    public static class HeapNode{
+        public int key;
+        public String info;
+        public HeapNode child;
+        public HeapNode next;
+        public HeapNode prev;
+        public HeapNode parent;
+        public int rank;
+        public boolean flag;
+        
+        public HeapNode link(HeapNode other) {
+        	return null;
+        }
+    }
+}
 
